955e2fd5fd7311cd56ae63c0e281f9558fd013d1
Dimpal Walde <114867886+Dimpal777@users.noreply.github.com>
2023-02-03 19:40:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,20 @@
 // We will understand all of this later in the course.
 // DO NOT MODIFY THIS FILE
 
-const msg = new SpeechSynthesisUtterance();
-  let voices = [];
-  const voicesDropdown = document.querySelector('[name="voice"]');
-  const options = document.querySelectorAll('[type="range"], [name="text"]');
-  const speakButton = document.querySelector('#speak');
-  const stopButton = document.querySelector('#stop');
+const express = require('express');
+const path = require('path');
 
+const app = express();
 
-speakButton.addEventListener('click', ()=>{
-    msg.rate = options[0].value;
-    msg.pitch = options[1].value;
-    msg.text = options[2].value;
+app.use(express.static(__dirname))
 
-speechSynthesis.speak(msg)
-})
-
-  stopButton.addEventListener('click', ()=>{
-    speechSynthesis.cancel();
-  });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+//your code here
+app.post('/add', (req, res) => {
+  const {a,b} = req.body;
+  res.status(200).send(a+b);
+  // res.sendFile(path.join(__dirname + '/main.html'));
+});
+module.exports = app;
